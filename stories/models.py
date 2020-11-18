1b365662a4a82d5d9867d5cf93a0e4da88feb356
Matt Westcott <matt@west.co.tt>
2020-11-18 18:06:14 +0000

    Example of StoryEmbedBlock on a blog page

--- stories/models.py
+++ stories/models.py
@@ -1,6 +1,24 @@
 from django.db import models
+from wagtail.admin.edit_handlers import StreamFieldPanel
+from wagtail.core import blocks
+from wagtail.core.fields import StreamField
+from wagtail.core.models import Page
+
+from wagtail_webstories.blocks import StoryEmbedBlock
 from wagtail_webstories.models import BaseWebStoryPage
 
 
 class StoryPage(BaseWebStoryPage):
     pass
+
+
+class BlogPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock()),
+        ('paragraph', blocks.RichTextBlock()),
+        ('story_embed', StoryEmbedBlock()),
+    ])
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
