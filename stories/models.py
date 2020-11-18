ee354a3c36cca2a1dc0d509518a25c27c48a06fe
Matt Westcott <matt@west.co.tt>
2020-11-18 21:04:13 +0000

    add carousel

--- stories/models.py
+++ stories/models.py
@@ -4,7 +4,7 @@ from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
-from wagtail_webstories.blocks import StoryEmbedBlock
+from wagtail_webstories.blocks import StoryChooserBlock, StoryEmbedBlock
 from wagtail_webstories.models import BaseWebStoryPage
 
 
@@ -17,6 +17,9 @@ class BlogPage(Page):
         ('heading', blocks.CharBlock()),
         ('paragraph', blocks.RichTextBlock()),
         ('story_embed', StoryEmbedBlock()),
+        ('carousel', blocks.StreamBlock([
+            ('story', StoryChooserBlock())
+        ], template="stories/blocks/carousel.html")),
     ])
 
     content_panels = Page.content_panels + [
