771c876160c03a53be006f3f7aa2134734c9bc4f
Matt Westcott <matt@west.co.tt>
2021-03-12 10:30:26 +0000

    Add example of embedding / linking external stories

--- stories/models.py
+++ stories/models.py
@@ -4,7 +4,7 @@ from wagtail.core import blocks
 from wagtail.core.fields import StreamField
 from wagtail.core.models import Page
 
-from wagtail_webstories.blocks import StoryChooserBlock, StoryEmbedBlock
+from wagtail_webstories.blocks import StoryChooserBlock, StoryEmbedBlock, ExternalStoryBlock, ExternalStoryEmbedBlock
 from wagtail_webstories.models import BaseWebStoryPage
 
 
@@ -23,9 +23,11 @@ class BlogPage(Page):
     body = StreamField([
         ('heading', blocks.CharBlock()),
         ('paragraph', blocks.RichTextBlock()),
-        ('story_embed', StoryEmbedBlock()),
+        ('story_embed', StoryEmbedBlock(label="Imported story")),
+        ('external_story', ExternalStoryEmbedBlock(label="External story")),
         ('carousel', blocks.StreamBlock([
-            ('story', StoryChooserBlock())
+            ('story', StoryChooserBlock(label="Imported story")),
+            ('external_story', ExternalStoryBlock(label="External story")),
         ], template="stories/blocks/carousel.html")),
     ])
 
